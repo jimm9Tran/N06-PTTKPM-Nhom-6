825d2199d0a2d55e90e179ce8e5656d8c3407aa3
jimm9 Tran <jimm9tran@gmail.com>
2025-02-09 18:03:40 +0700

    hero

--- SRC/front-end/src/App.jsx
+++ SRC/front-end/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </>
   )
 }
---
+++ SRC/front-end/src/components/Hero/Hero.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import Slider from "react-slick";
+import Image1 from "../../assets/hero/air-jordan-1-low-paris.jpeg";
+
+const HeroData = [
+  {
+    id: 1,
+    img: Image1,
+    subtitle: "Nike Air Jordan 1",
+    title: "Jordan 1",
+    title2: "Shoe",
+    description: "nothing to write"
+  },
+  {
+    id: 2,
+    img: Image1,
+    subtitle: "Nike Air Jordan 1",
+    title: "Jordan 1",
+    title2: "Shoe",
+    description: "nothing to write"
+  },
+  {
+    id: 3,
+    img: Image1,
+    subtitle: "Nike Air Jordan 1",
+    title: "Jordan 1",
+    title2: "Shoe",
+    description: "nothing to write"
+  }
+]
+
+const Hero = () => {
+
+  const settings = {
+      dots: false,
+      arrows: false,
+      infinite: true,
+      speed: 800,
+      slidesToScroll: 1,
+      autoplaySpeed: 4000,
+      cssEase: "ease-in-out",
+      pauseOnHover: false,
+      pauseOnFocus: true,
+  };
+
+  return (
+    <div className='container'>
+        <div className='overflow-hidden rounded-3xl min-h-[550px] sm:min-h-[650px] hero-bg-color flex justify-center items-center'>
+          <div className='container pb-8 sm:pb-0'>
+            {/* Hero section  */}
+            <Slider {...settings}>
+              {
+                HeroData.map((data) => (
+                  <div key={data.id}>
+                    <div className='grid grid-cols-1 sm:grid-cols-2'>
+                      {/* Text content section  */}
+                      <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1'>
+                        <h1>{data.subtitle}</h1>
+                        <h1>{data.title}</h1>
+                        <h1>{data.title2}</h1>
+                      </div>
+
+                      <div>
+                        <button>
+                          shop now
+                        </button>
+                      </div>
+                        {/* Img section  */}
+                      <div>
+                        <div>
+                          <img 
+                            src={data.img} 
+                            alt={data.subtitle} 
+                            className='w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110
+                              object-contain mx-auto drop-shadow-[ -9px_4px_6px_rgba(0,0,0,.4)]'
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </Slider>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
